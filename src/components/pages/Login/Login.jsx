e3ac4838389007e0fc65c933067308413ff4a753
Mary <32392607+MartFrida@users.noreply.github.com>
2024-02-03 17:15:34 +0100

    delete unused useNavigation

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
 import { loginThunk } from '../../../redux/auth/operations'
-import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 
@@ -13,14 +12,12 @@ const Login = () => {
       password: 'marisata@email.com',
     }
   })
-  const navigate = useNavigate()
   const dispatch = useDispatch()
   const submit = data => {
     console.log(data)
     dispatch(loginThunk(data))
       .unwrap()
       .then((res) => {
-        // navigate('/articles')
         toast.success(`Welcome ${res.user.name}`)
       })
       .catch(() => { toast.error('Something went wrong!') })
