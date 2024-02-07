ba9fc2fcd6e5ca187900c32994c8889e30afcd13
Mary <32392607+MartFrida@users.noreply.github.com>
2024-02-07 20:45:12 +0100

    stiled login/register components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -24,17 +24,17 @@ const Login = () => {
   }
 
   return (
-    <div className='flex justify-center items-center min-h-[80vh]'>
-      <form onSubmit={handleSubmit(submit)} className='grid gap-4 border-2 border-black rounded-md shadow-md px-10 py-14'>
+    <div className='flex justify-center items-center min-h-screen px-6 py-12 lg:px-8'>
+      <form onSubmit={handleSubmit(submit)} className='bg-white grid gap-4 shadow-lg border border-gray-300 rounded-md px-10 py-14 min-w-80'>
         <label className='flex flex-col gap-2'>
           <span>Email</span>
-          <input {...register('email')} placeholder='Enter the email' className='border-2 border-black rounded-md p-1' type='email' />
+          <input {...register('email')} placeholder='Enter the email' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' type='email' />
         </label>
         <label className='flex flex-col gap-2'>
           <span>Password</span>
-          <input {...register('password')} placeholder='Enter the password' className='border-2 border-black rounded-md p-1' type='password' />
+          <input {...register('password')} placeholder='Enter the password' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' type='password' />
         </label>
-        <button className='border-2 border-black rounded-md px-4 py-2' >Login</button>
+        <button className='shadow-lg w-full rounded border-2 bg-white px-3 py-3 text-base font-bold text-gray-900 transition duration-100 hover:shadow-gray-300 mt-2' >Login</button>
       </form>
     </div>
   )
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,31 +2,36 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
 import { registerThunk } from '../../redux/auth/operations'
+import { toast } from 'react-toastify'
 
 const Register = () => {
   const { register, handleSubmit } = useForm()
   const dispatch = useDispatch()
   const submit = data => {
-    console.log(data)
     dispatch(registerThunk(data))
+      .unwrap()
+      .then((res) => {
+        toast.success(`Welcome ${res.user.name}`)
+      })
+      .catch(() => { toast.error('Something went wrong!') })
   }
 
   return (
-    <div className='flex justify-center items-center min-h-[80vh]'>
-      <form onSubmit={handleSubmit(submit)} className='grid gap-4 border-2 border-black rounded-md shadow-md px-10 py-14'>
+    <div className='flex justify-center items-center min-h-screen px-6 py-12 lg:px-8'>
+      <form onSubmit={handleSubmit(submit)} className='bg-white grid gap-4 shadow-lg border border-gray-300 rounded-md px-10 py-14 min-w-80'>
         <label className='flex flex-col gap-2'>
           <span>Name</span>
-          <input {...register('name')} placeholder='Enter the name' className='border-2 border-black rounded-md p-1' type='text' />
+          <input {...register('name')} placeholder='Enter the name' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' type='text' />
         </label>
         <label className='flex flex-col gap-2'>
           <span>Email</span>
-          <input {...register('email')} placeholder='Enter the email' className='border-2 border-black rounded-md p-1' type='email' />
+          <input {...register('email')} placeholder='Enter the email' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' type='email' />
         </label>
         <label className='flex flex-col gap-2'>
           <span>Password</span>
-          <input {...register('password')} placeholder='Enter the password' className='border-2 border-black rounded-md p-1' type='password' />
+          <input {...register('password')} placeholder='Enter the password' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 ' type='password' />
         </label>
-        <button className='border-2 border-black rounded-md px-4 py-2' >Register</button>
+        <button className='shadow-lg w-full rounded border-2 bg-white px-3 py-3 text-base font-bold text-gray-900 transition duration-100 hover:shadow-gray-300 mt-2' >Register</button>
       </form>
     </div>
   )
